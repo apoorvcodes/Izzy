60143b8bb978afce6ce09427b8fda240f8961945
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-04-07 20:34:01 +0530

    small changes

---
+++ commands/tofu.js
@@ -0,0 +1,16 @@
+//sooon 
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "tofu",
+    description: "Info about me :)", //you can remove this and your info here! pull request appricieated
+
+    async run (client, message, args) {
+
+      
+    
+
+
+     
+    }
+}
